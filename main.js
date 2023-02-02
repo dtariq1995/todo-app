8527661a216918bffa0eb587d0b08c46f81667d4
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2023-02-02 01:45:23 -0600

    added styling to header and footer

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("Hello");
+(()=>{"use strict";!function(){let e=document.querySelector("#content"),t=document.querySelector("body"),n=document.createElement("header");n.textContent="Header",t.insertAdjacentElement("afterbegin",n);let r=document.createElement("footer"),a=document.createElement("p"),c=document.createElement("a"),o=document.createElement("img");a.textContent="Created by:",o.src="/src/Assets/Images/GitHub-Mark-Light-32px.png",c.href="https://github.com/dtariq1995",c.textContent="Daanyaal Tariq",r.append(a,o,c),e.insertAdjacentElement("afterend",r)}()})();
