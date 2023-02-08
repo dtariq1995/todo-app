2f931abdda92014c4b183d3bcd757cc9b6e2e315
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2023-02-08 13:26:54 -0600

    Added Icons for different sidebar sections

--- src/Pages/sidebarLoad.js
+++ src/Pages/sidebarLoad.js
@@ -6,22 +6,42 @@ const sidebarLoad = function() {
     let sidebar = document.createElement("div");
     sidebar.id = "sidebar";
 
+    let homeArea = document.createElement("div");
+    let homeImg = document.createElement("img");
+    homeImg.src = "/src/Assets/Images/houseicon.png";
     let homeButton = document.createElement("button");
     homeButton.textContent = "Home";
+    homeArea.append(homeImg, homeButton);
 
+    let todayArea = document.createElement("div");
+    let todayImg = document.createElement("img");
+    todayImg.src = "/src/Assets/Images/dayicon.png";
     let todayButton = document.createElement("button");
     todayButton.textContent = "Today";
+    todayArea.append(todayImg, todayButton);
 
+    let weekArea = document.createElement("div");
+    let weekImg = document.createElement("img")
+    weekImg.src = "/src/Assets/Images/weekicon.png";
     let weekButton = document.createElement("button");
     weekButton.textContent = "Week";
+    weekArea.append(weekImg, weekButton);
 
+    let projectsArea = document.createElement("div");
+    let projectsImg = document.createElement("img");
+    projectsImg.src = "/src/Assets/Images/projectsicon.png";
     let projectsButton = document.createElement("button");
     projectsButton.textContent = "Projects";
+    projectsArea.append(projectsImg, projectsButton);
 
+    let notesArea = document.createElement("div");
+    let notesImg = document.createElement("img");
+    notesImg.src = "/src/Assets/Images/noteicon.png";
     let notesButton = document.createElement("button");
     notesButton.textContent = "Notes";
+    notesArea.append(notesImg, notesButton);
 
-    sidebar.append(homeButton, todayButton, weekButton, projectsButton, notesButton);
+    sidebar.append(homeArea, todayArea, weekArea, projectsArea, notesArea);
     content.append(sidebar);
 
 }
