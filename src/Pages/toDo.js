381ecf8020d453139de16b48f828fb48100bb672
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2023-03-10 15:41:21 -0600

    added display for todos

--- src/Pages/toDo.js
+++ src/Pages/toDo.js
@@ -1,3 +1,5 @@
+import format from "date-fns/format";
+
 const toDoArray = [];
 
 const toDo = (function() {
@@ -8,10 +10,12 @@ const toDo = (function() {
         return {title, project, priority, date, details};
     };
 
-    const sampleToDo = toDoFactory("Title", "Project", "High", "Today", "Finish this");
-    const sampleToDoTwo = toDoFactory("Kill Ali", "Daily Tasks", "High", "Today", "This needs to be done immediately, by any means necessary");
+    const sampleToDo = toDoFactory("Title", "Project", "High", new Date(2023, 2, 8), "Finish this");
+    const sampleToDoTwo = toDoFactory("Kill Ali", "Daily Tasks", "Medium", new Date(2023, 2, 8), "This needs to be done immediately, by any means necessary");
+    const sampleToDoThree = toDoFactory("Kill Shams", "Daily Tasks", "Easy", new Date(2023, 2, 8), "This idiot can be useful so this can wait");
 
-    toDoArray.push(sampleToDo, sampleToDoTwo, sampleToDo, sampleToDoTwo);
+
+    toDoArray.push(sampleToDo, sampleToDoTwo, sampleToDoThree, sampleToDo, sampleToDoTwo, sampleToDoThree);
     console.log(toDoArray);
 })();
 
@@ -29,15 +33,41 @@ const homeDisplay = function() {   // display all to-dos
         let toDoCard = document.createElement("div");
         toDoCard.classList.add("todo-card");
 
+        if (toDo.priority === "High") {
+            toDoCard.style.borderLeft = "3px solid red";
+        }
+        else if (toDo.priority === "Medium") {
+            toDoCard.style.borderLeft = "3px solid orange";
+        }
+        else {
+            toDoCard.style.borderLeft = "3px solid green";
+        }
+
         let toDoTitle = document.createElement("div");
         toDoTitle.textContent = toDo.title;
         toDoTitle.classList.add("todo-title");
 
-        let toDoDetails = document.createElement("div");
-        toDoDetails.textContent = toDo.details;
-        toDoDetails.classList.add("todo-details");
+        let toDoLeft = document.createElement("div");
+        let toDoRight = document.createElement("div");
+        toDoRight.classList.add("todo-right");
 
-        toDoCard.append(toDoTitle, toDoDetails);
+        let toDoDate = document.createElement("div");
+        toDoDate.classList.add("todo-date");
+        toDoDate.textContent = format(toDo.date, 'LLL do');
+
+        let editButton = document.createElement("object");
+        editButton.type = "image/svg+xml";
+        editButton.data = "/src/Assets/Images/edit-button.svg";
+        editButton.classList.add("todo-icon");
+
+        let deleteButton = document.createElement("object");
+        deleteButton.type = "image/svg+xml";
+        deleteButton.data = "/src/Assets/Images/trash-can.svg";
+        deleteButton.classList.add("todo-icon");
+
+        toDoLeft.append(toDoTitle);
+        toDoRight.append(toDoDate, editButton, deleteButton);
+        toDoCard.append(toDoLeft, toDoRight);
         toDoDisplayArea.append(toDoCard);
 
     });
