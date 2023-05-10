f0a3098cbf72e02496eb0beefda9d231cc29af2f
dtariq1995 <77370572+dtariq1995@users.noreply.github.com>
2023-05-10 14:24:42 -0500

    fixed issue with htmlwebpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './styles.css';
 import pageLoad from "./Pages/pageLoad";
 
+let body = document.body;   // add some things onto html body which everything else will be attached to
+let container = document.createElement("container");
+container.id = "dim";
+let content = document.createElement("div");
+content.id = "content";
+container.append(content);
+let form = document.createElement("form");
+form.id = "edit-form";
+body.append(container, form);
+
 pageLoad();
